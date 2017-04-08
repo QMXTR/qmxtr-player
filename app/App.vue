a04bf94c5766a984d5fef1aeb1c97ba71ae89d58
HelloWorld017 <deu07115@gmail.com>
2017-04-09 00:02:29 +0900

    Initial Commit

---
+++ app/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="app">
+		<q-workspace>
+			<q-panel>
+				<q-tab>
+					<q-tab-panel title="Album Arts">
+						<q-aa-panel></q-aa-panel>
+					</q-tab-panel>
+				</q-tab>
+			</q-panel>
+		</q-workspace>
+		<q-aside>
+			<q-panel>
+				<q-playlist></q-playlist>
+			</q-panel>
+		</q-aside>
+		<q-controlbar>
+			<q-panel>
+				<q-controller></q-controller>
+				<q-seekbar></q-seekbar>
+			</q-panel>
+		</q-controlbar>
+	</div>
+</template>
+
+<style lang="less">
+	body, html {
+		margin: 0;
+		padding: 0;
+	}
+
+	::selection {
+		background: #1de9b6;
+	}
+</style>
+
+<script>
+	export default {};
+</script>
---
+++ app/index.js
@@ -0,0 +1,86 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import Qmxtr from "qmxtr";
+import EqualizerPlugin from "qmxtr-equalizer";
+import KeyInputPlugin from "qmxtr-keyinput";
+import VolumePlugin from "qmxtr-volume";
+
+import App from "./App.vue";
+
+const qmxtr = {};
+console.log(Qmxtr);
+
+qmxtr.equalizer = new EqualizerPlugin({
+	60: 1,
+	170: 1,
+	310: 1,
+	600: 1,
+	1000: 1,
+	3000: 1,
+	6000: 1,
+	12000: 1,
+	14000: 1,
+	16000: 1
+});
+
+qmxtr.volume = new VolumePlugin(1, 1);
+qmxtr.keyInput = new KeyInputPlugin({});
+
+qmxtr.vis = document.createElement('div');
+qmxtr.player = new Qmxtr.QPlayer({
+	visualizer: qmxtr.vis,
+	plugins: [
+		qmxtr.volume,
+		qmxtr.equalizer,
+		qmxtr.keyInput
+	]
+});
+
+Vue.use(Vuex);
+Vue.use(Qmxtr.QmxtrUI);
+
+qmxtr.store = new Vuex.Store({
+	state: qmxtr.player.defaultStates,
+	mutations: qmxtr.player.defaultMutations,
+	actions: qmxtr.player.defaultActions
+});
+
+qmxtr.player.attachToVuexStore(qmxtr.store);
+
+qmxtr.app = new Vue({
+	el: '#app',
+	render(h){
+		return h(App);
+	},
+	store: qmxtr.store
+});
+
+window.addEventListener('load', () => {
+	const register = () => setTimeout(() => {
+		const visualizer = document.querySelector('#visualizer');
+		if(!visualizer) return register();
+		visualizer.appendChild(qmxtr.vis);
+	}, 500);
+
+	register();
+}, false);
+
+qmxtr.player.addToQueue({
+	src: '/test/audio/000.mp3',
+	title: 'Heikousen',
+	author: 'Sayuri'
+});
+
+qmxtr.player.addToQueue({
+	src: '/test/audio/001.mp3',
+	title: 'Shelter',
+	author: 'Porter Robinson'
+});
+
+qmxtr.player.addToQueue({
+	src: '/test/audio/002.mp3',
+	title: 'Marble Soda',
+	author: 'Shawn Wasabi'
+});
+
+window.qmxtr = qmxtr;
